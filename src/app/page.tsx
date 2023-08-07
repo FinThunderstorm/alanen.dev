73c64d81a284725b45084a5f5e1d77a1014ae16a
Tuomas Alanen <tuomas@alanen.dev>
2023-08-07 19:15:58 +0300

    Create home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
-import Image from 'next/image'
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="font-sans">
+      <div className="py-2">
+        <p>👋🏻 Hi over there, my name is </p>
+        <h1 className="font-serif font-semibold text-6xl uppercase text-green-500">
+          Tuomas Alanen.
+        </h1>
+        <p className="text-2xl font-semibold">
+          I'm a Web Form Service Artisan.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      </div>
+      <div className="py-2">
+        <p>
+          I am a{" "}
+          <span className="font-bold text-green-500">software developer</span>{" "}
+          who loves to make an impact on users' life by solving identified
+          problems. I'm passionate about{" "}
+          <span className="font-bold text-green-500">
+            fullstack development
+          </span>
+          , learning analytics and I also have done some designing and material
+          creation for existing platforms. I also want to{" "}
+          <span className="font-bold text-green-500">identify problems</span>{" "}
+          and find the root cause for identified problems before solving them. I
+          love doing things in{" "}
+          <span className="font-bold text-green-500">agile manner</span> and
+          don't hesitate to take the software into new directions.
+        </p>
+      </div>
+      <div className="py-2">
+        <p>
+          👨🏻‍💻 My current position is{" "}
+          <span className="font-bold text-green-500">
+            Trainee Software Developer
+          </span>{" "}
+          at <a href="https://digia.com/en/">Digia Plc</a>. My past work history
+          is available on my{" "}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="text-green-500"
+            href="https://www.linkedin.com/in/tuomasalanen/"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            LinkedIn profile
+          </a>
+          .
+        </p>
+        <p>
+          📖 I'm Bachelor of Computer Science Student at University of Helsinki.
+        </p>
+        <p>
+          🐴 My main work horses are TypeScript, SQL, Node.js, Python, Bash,
+          React.js, Docker and AWS.
+        </p>
+        <p>
+          📚 I have Certified ScrumMaster® (CSM®) and Certified Scrum Product
+          Owner® (CSPO®) certifications from Scrum Alliance. Also I am AWS
+          Certified Solutions Architect - Associate.
+        </p>
+      </div>
+      <div className="py-2">
+        <h2>🏃🏻‍♂️ You can find me on</h2>
+        <div className="flex flex-row gap-2">
+          <a
+            className="text-green-500"
+            href="https://www.linkedin.com/in/tuomasalanen/"
+          >
+            linkedin
+          </a>
+          <a
+            className="text-green-500"
+            href="https://github.com/FinThunderstorm"
+          >
+            github
+          </a>
+          <a
+            className="text-green-500"
+            href="https://twitter.com/finthunderstorm"
+          >
+            twitter
+          </a>
+          <a className="text-green-500" href="mailto:tuomas@alanen.dev">
+            email
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
 }
+
+export default Home
